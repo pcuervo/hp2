acfb0dd34301ec9ed341dbf042d80e9d3ef37943
maichomper <maichomper@gmail.com>
2014-06-05 19:21:02 -0500

    cambios header index

--- js/registro.js
+++ js/registro.js
@@ -4,8 +4,28 @@
 
 	$(function(){
 
-		$('#forma-registro').validate();
+		$('#forma-registro').validate({
+			submitHandler: function(){
+				enviarFormaAJAX();
+			} 
+		});	
 
 	});
+
+	function enviarFormaAJAX(){
+		$('form').submit(function(e){
+            e.preventDefault();
+            $.ajax({
+                type:       "POST",
+                url:        "php/RegistraInfo.php", 
+                data:       $( "form" ).serialize(),
+                success:    function(data){
+                    data_json = $.parseJSON(data);
+                    alert("Gracias por registrarte " + data_json.nombre);
+                    window.location = "registro.html";
+                }
+            });
+        });
+	}
 
 })(jQuery);
